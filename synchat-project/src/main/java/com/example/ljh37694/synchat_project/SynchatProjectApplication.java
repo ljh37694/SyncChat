a78c784bc96e597c969211f651c8de60fa044534
i_junghoon <84302688+ljh37694@users.noreply.github.com>
2025-03-15 21:32:41 +0900

    spring server init

---
+++ synchat-project/src/main/java/com/example/ljh37694/synchat_project/SynchatProjectApplication.java
@@ -0,0 +1,12 @@
+package com.example.ljh37694.synchat_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynchatProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynchatProjectApplication.class, args);
+	}
+}
---
+++ synchat-project/src/test/java/com/example/ljh37694/synchat_project/SynchatProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ljh37694.synchat_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynchatProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
